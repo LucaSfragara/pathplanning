5a080b67b59a2963fc7fe418ed23d7e9aacceaf1
lucamelfi18 <lmelfi@andrew.cmu.edu>
2025-02-18 21:29:48 -0500

    hehe

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 from motorgo import Plink, ControlMode, BrakeMode
 import time
 import math
+from odometry import nav_straight
+from odometry import turn_in_place_dumb
+
+
+plink = Plink()
+    
+left_motor = plink.channel1
+right_motor = plink.channel2
+left_motor.control_mode = ControlMode.VELOCITY
+right_motor.control_mode = ControlMode.VELOCITY
+left_motor.set_velocity_pid_gains(6, 1.4, 0.1)
+right_motor.set_velocity_pid_gains(6, 1.4, 0.1)
+
+plink.connect()
+
+vertices = [(10, 10), (15, 10), (15, 15)]
+currX, currY = 0, 0
+curr_angle = math.pi/2
+
+for goalX, goalY in vertices:
+
+    currX, currY, curr_angle = turn_in_place_dumb(currX, currY, curr_angle, goalX, goalY)
+    currX, currY, curr_angle = nav_straight(currX, currY, curr_angle, goalX, goalY)
+
